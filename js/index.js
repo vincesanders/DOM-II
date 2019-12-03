25a4d136c3d3e7bf5968bffa02688a0632c24625
Vincent Sanders <vincesanders@hotmail.com>
2019-12-03 16:10:14 -0600

    Added mousemove, animationend and click animations to cursor.

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const images = document.querySelectorAll('img');
 const tablet = window.matchMedia("(max-width: 800px)");
 const bannerImage = document.querySelector('header img');
 const middleCards = document.querySelectorAll('.content-section');
+const cursor = document.getElementById('cursor');
 
 images.forEach(img => {                                            //Event 1
     img.addEventListener('mouseenter', e => {
@@ -30,4 +31,19 @@ window.addEventListener('resize', e => {                           //Event 3
     } else {
         bannerImage.src = 'img/fun-bus.jpg';
     }
+});
+
+window.addEventListener('mousemove', e => {                        //Event 6
+    cursor.style.top = e.clientY - 25 + 'px';
+    cursor.style.left = e.clientX - 25 + 'px';
+});
+
+document.addEventListener('click', e => {                         //Event 7
+    let circle = document.createElement("circle");
+    cursor.appendChild(circle);
+    circle.style.top = e.clientY + "px";
+    circle.style.left = e.clientX + "px";
+    circle.addEventListener('animationend', e => {                //Event 8
+        e.target.parentElement.removeChild(circle);
+    });
 });
