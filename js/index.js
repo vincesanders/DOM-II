5d96fbe16458a77b96c00407b810b45d6ed04b30
Vincent Sanders <vincesanders@hotmail.com>
2019-12-03 10:17:44 -0600

    Added 2 event listeners to the Fun Bus images.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const images = document.querySelectorAll('img');
+
+images.forEach((img) => {
+    img.addEventListener('mouseenter', (e) => {
+        e.target.style.transform = 'scale(1.2)';
+        e.target.style.transition = 'all 0.3s';
+    });
+    img.addEventListener('mouseleave', (e) => {
+        e.target.style.transform = 'scale(1)';
+        e.target.style.transition = 'all 0.3s';
+    });
+});
