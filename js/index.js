63f7fcd9ae8c8365063130c6080736d05398cebe
Vincent Sanders <vincesanders@hotmail.com>
2019-12-03 14:14:58 -0600

    Added a 4th event that rotates the images on click.

--- js/index.js
+++ js/index.js
@@ -3,18 +3,20 @@ const images = document.querySelectorAll('img');
 const tablet = window.matchMedia("(max-width: 800px)");
 const bannerImage = document.querySelector('header img');
 
-images.forEach((img) => {
-    img.addEventListener('mouseenter', (e) => {
+images.forEach(img => {                                            //Event 1
+    img.addEventListener('mouseenter', e => {
         e.target.style.transform = 'scale(1.2)';
         e.target.style.transition = 'all 0.3s';
     });
-    img.addEventListener('mouseleave', (e) => {
+    img.addEventListener('mouseleave', e => {                      //Event 2
         e.target.style.transform = 'scale(1)';
-        e.target.style.transition = 'all 0.3s';
+    });
+    img.addEventListener('click', e => {
+        e.target.style.transform = 'rotateY(180deg)';              //Event 4
     });
 });
 
-window.addEventListener('resize', (e) => {
+window.addEventListener('resize', e => {                           //Event 3
     if (tablet.matches) {
         bannerImage.src = 'img/bus-and-kayaks-on-beach.jpg';
     } else {
